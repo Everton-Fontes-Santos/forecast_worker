f2c47ba2a2bd3e9f93979eeb8ac0faedf18101f0
Everton Fontes <efs.fontes@gmail.com>
2023-01-30 11:40:06 -0300

    stop the queue

--- index.js
+++ index.js
@@ -19,32 +19,42 @@ const main = async ()=>{
     if(send) return
     send=true
     const dataToSend = [[]]
-    const stock = new Stocks(process.env.API_KEY)
-    const tickers = await stock.timeSeries({
-        symbol: 'EURUSD',
-        interval: stock.INTERVALS[2],
-        amount: 15
-       });
-    tickers.forEach((ticker)=>{
-        dataToSend[0].push([ticker.close])
-    })
-    dataToSend[0].push([0])
-    const response = await axios.post(
-        'https://tensor-serving.onrender.com/v1/models/saved_model:predict',
-        { "instances": dataToSend}
-    )
     
-    const data = response?.data
-    if(!data) return
-    const prediction = data.predictions[0][0]
-    const scaledPrediction = forecast.m15.inverseScale(prediction)
-    const last_close = tickers.at(0).close
-    await queue.publish(process.env.CHANNEL, {
-        forecast: scaledPrediction,
-        last_close,
-        timeframe
-    })
+    const stock = new Stocks(process.env.API_KEY)
+    try{
+        const tickers = await stock.timeSeries({
+            symbol: 'EURUSD',
+            interval: stock.INTERVALS[2],
+            amount: 15
+           });
+        
+           tickers.forEach((ticker)=>{
+            dataToSend[0].push([ticker.close])
+        })
+        dataToSend[0].push([0])
+        const response = await axios.post(
+            'https://tensor-serving.onrender.com/v1/models/saved_model:predict',
+            { "instances": dataToSend}
+        )
+        
+        const data = response?.data
+        if(!data) return
+        const prediction = data.predictions[0][0]
+        const scaledPrediction = forecast.m15.inverseScale(prediction)
+        const last_close = tickers.at(0).close
+        await queue.publish(process.env.CHANNEL, {
+            forecast: scaledPrediction,
+            last_close,
+            timeframe
+        })
+
+        process.exit()
+    }catch(err){
+        console.log(err)
+        process.exit()
+    }    
 }
 console.log("Starting worker")
 // setInterval(main, 1000);
 main()
+// 
